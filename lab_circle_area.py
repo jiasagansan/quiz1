00d9538400072d578fe0bf6972d7c44d3d8cb238
jiasagansan <sagansanjia20@.com>
2023-10-12 20:46:01 +0800

    first commit

---
+++ lab_circle_area.py
@@ -0,0 +1,12 @@
+# This program will compute the circle of area based on its radius
+PI = 3.14159
+
+radius = int(input("Enter radius value: "))
+
+area = radius * radius * PI
+
+ # Display Result
+print("The area of circle with radius", radius, "is", area)
+
+
+  
